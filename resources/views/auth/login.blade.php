e96f41a9badf06a0b2e0d0568b94b7fc25cc19de
Cesar Aquino <cesar@ismem.edu.pe>
2022-11-16 21:56:33 -0500

    vistas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
---
+++ resources/views/clientes.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Clientes') }}
+        </h2>
+        <a href="#" class="btn btn-primary registrar">Registrar cliente</a>
+    </x-slot>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            
+
+            <table class="table">
+              <tr>
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>Telefono</th>
+                <th>Email</th>
+                <th>Acciones</th>
+              </tr>
+              <tr>
+                <td>Alex</td>
+                <td>Valera</td>
+                <td>85296325</td>
+                <td>valera@cocoroco.com</td>
+                <td>
+                  <a href="#" class="btn btn-success">Editar</a>
+                  <a href="#" class="btn btn-danger">Eliminar</a>
+                </td>
+              </tr>
+              <tr>
+                <td>Alex</td>
+                <td>Valera</td>
+                <td>85296325</td>
+                <td>valera@cocoroco.com</td>
+              </tr>
+              <tr>
+                <td>Alex</td>
+                <td>Valera</td>
+                <td>85296325</td>
+                <td>valera@cocoroco.com</td>
+              </tr>
+              <tr>
+                <td>Alex</td>
+                <td>Valera</td>
+                <td>85296325</td>
+                <td>valera@cocoroco.com</td>
+              </tr>
+            </table>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+</x-app-layout>
